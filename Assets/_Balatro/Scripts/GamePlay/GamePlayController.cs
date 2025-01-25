28cf44da9502b4d9351ba3f7ddf6bc29e1f49ca4
ttthien27 <52747168+ttthien27@users.noreply.github.com>
2025-01-25 18:22:36 +0700

    Sort Card by Rank and Suit

--- Assets/_Balatro/Scripts/GamePlay/GamePlayController.cs
+++ Assets/_Balatro/Scripts/GamePlay/GamePlayController.cs
@@ -1,6 +1,9 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Balatro;
+using DG.Tweening;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 namespace GamePlay
 {
@@ -18,7 +21,7 @@ namespace GamePlay
 
         public Action OnWin;
         public Action OnLose;
-        
+
         public void BackToMainMenu()
         {
             OnLose?.Invoke();
@@ -36,23 +39,99 @@ namespace GamePlay
             var cards = _playerHandCard.Cards;
             var cardsSorted = SortCardByRank(cards);
             _playerHandCard.Sort(cardsSorted);
+            UpdateCardPositionsAfterSort(cardsSorted);
         }
-        
+
         public void SortCardBySuit()
         {
             var cards = _playerHandCard.Cards;
             var cardsSorted = SortCardBySuit(cards);
             _playerHandCard.Sort(cardsSorted);
+            UpdateCardPositionsAfterSort(cardsSorted);
         }
 
         public List<Card> SortCardByRank(List<Card> cards)
         {
-            return cards;
+            var sortedCards = cards.OrderBy(card => card.GetCardData().Value == 1 ? int.MaxValue : card.GetCardData().Value)
+                       .ThenBy(card => (CardType)card.GetCardData().Type)
+                       .ToList();
+
+
+            List<CardSlot> originalSlots = GetListCardSlotByPositionX(cards);
+
+            for (int i = 0; i < sortedCards.Count; i++)
+            {
+                var originalSlot = originalSlots[i];
+
+                sortedCards[i].SetCardSlot(originalSlot);
+            }
+
+            return sortedCards;
         }
+
+        public void UpdateCardPositionsAfterSort(List<Card> sortedCards)
+        {
+            for (int i = 0; i < sortedCards.Count; i++)
+            {
+                var card = sortedCards[i];
+                var newPos = card.GetCardSlot().CardPosition;
+                card.SetupOriginTransform(newPos, card.GetCardSlot().CardLocalRotation);
+            }
+        }
+
 
         public List<Card> SortCardBySuit(List<Card> cards)
         {
+            var sortedCards = cards.OrderBy(card => GetCardTypePriority((CardType)card.GetCardData().Type))
+                       .ThenBy(card => card.GetCardData().Value == 1 ? int.MaxValue : card.GetCardData().Value)
+                       .ToList();
+
+
+            List<CardSlot> originalSlots = GetListCardSlotByPositionX(cards);
+
+            for (int i = 0; i < sortedCards.Count; i++)
+            {
+                var originalSlot = originalSlots[i];
+
+                sortedCards[i].SetCardSlot(originalSlot);
+            }
+
             return cards;
+        }
+
+        private int GetCardTypePriority(CardType type)
+        {
+            switch (type)
+            {
+                case CardType.Spades:
+                    return 0;
+                case CardType.Hearts:
+                    return 1;
+                case CardType.Clubs:
+                    return 2;
+                case CardType.Diamonds:
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+
+        public List<Card> SortCardsByPositionX(List<Card> cards)
+        {
+            var sortedCards = cards.OrderBy(card => card.transform.position.x).ToList();
+
+            return sortedCards;
+        }
+
+        public List<CardSlot> GetListCardSlotByPositionX(List<Card> cards)
+        {
+            var cardsByPositionX = SortCardsByPositionX(cards);
+            List<CardSlot> originalSlots = new List<CardSlot>();
+            foreach (var card in cardsByPositionX)
+            {
+                originalSlots.Add(card.GetCardSlot());
+            }
+            return originalSlots;
         }
     }
 }
